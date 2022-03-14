95a82a484e48d4ea3f1b45dce26c948c46243695
Mendrika27 <98638405+Mendrika27@users.noreply.github.com>
2022-03-14 14:29:20 +0300

    Update sum.js

--- sum.js
+++ sum.js
@@ -1,11 +1,14 @@
+
+//here are two fractions: first-second and third-fourth
 function sum (first, second, third, fourth){
     
+    //if our two fractions have the same denominator...
     if ( second == fourth){
         return (first + third)/ second;
     }
+    
+    //else...
     if(second != fourth) {
         return ((first * fourth)+(third * second)/(second * fourth));
     }
-
 }
-console.log(sum(1, 2, 3, 2));
